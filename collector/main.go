5fab844753ae35f2456499146b6e1876ed966c47
derktes <doublehelix9@gmail.com>
2020-04-13 18:40:50 +0800

    Basic IR signal collector CLI without HTTP capability

---
+++ collector/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+
+	"github.com/tarm/serial"
+)
+
+func main() {
+
+	config := &serial.Config{Name: "/dev/ttyACM0", Baud: 9600}
+	port, err := serial.OpenPort(config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lineScanner := bufio.NewScanner(io.Reader(port))
+
+	headers := make([]string, 0)
+	records := make([][]string, 0)
+	fields := make([]string, 0)
+
+	go func() {
+		signalChannel := make(chan os.Signal)
+		signal.Notify(signalChannel, os.Interrupt)
+
+		fmt.Println("Press Ctrl-C to exit program")
+
+		<-signalChannel
+
+		fmt.Println("Saving data")
+
+		file, err := os.Create("output.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		outputWriter := csv.NewWriter(file)
+		err = outputWriter.Write(headers)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = outputWriter.WriteAll(records)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		outputWriter.Flush()
+		file.Close()
+		port.Close()
+		os.Exit(0)
+	}()
+
+	headersCreated := false
+	messageHeaderSent := false
+
+	for lineScanner.Scan() {
+
+		line := lineScanner.Text()
+		fmt.Println(line)
+
+		if strings.HasPrefix(line, "Decoded") {
+
+			valueScanner := bufio.NewScanner(strings.NewReader(line))
+			valueScanner.Split(bufio.ScanWords)
+			for valueScanner.Scan() {
+				token := valueScanner.Text()
+				if strings.HasPrefix(token, "Value") {
+					dataValues := strings.Split(token, ":")
+					fields = append(fields, dataValues[1])
+					if !headersCreated {
+						headers = append(headers, "value")
+					}
+				}
+			}
+			messageHeaderSent = true
+
+		} else if strings.Contains(line, "Head") {
+
+			leftTrimmedLine := strings.TrimLeft(line, " \t")
+			token := strings.Split(leftTrimmedLine, " ")
+			if !headersCreated {
+				headers = append(headers, "hm")
+				headers = append(headers, "hs")
+			}
+			fields = append(fields, strings.TrimLeft(token[1], "m"))
+			fields = append(fields, strings.TrimLeft(token[3], "s"))
+
+		} else if messageHeaderSent && (strings.HasPrefix(line, "0:") ||
+			strings.HasPrefix(line, "4:") ||
+			strings.HasPrefix(line, "8:") ||
+			strings.HasPrefix(line, "12:") ||
+			strings.HasPrefix(line, "16:") ||
+			strings.HasPrefix(line, "20:") ||
+			strings.HasPrefix(line, "24:") ||
+			strings.HasPrefix(line, "28:")) {
+
+			valueScanner := bufio.NewScanner(strings.NewReader(line))
+
+			valueScanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+				trimmedData := bytes.TrimLeft(data, " \t")
+				sepIndex := bytes.IndexAny(trimmedData, ":\t")
+				if sepIndex > -1 {
+					return sepIndex + 1 + (len(data) - len(trimmedData)), trimmedData[:sepIndex], nil
+				}
+				return 0, nil, nil
+			})
+
+			for valueScanner.Scan() {
+				token := valueScanner.Text()
+				token = strings.TrimSpace(token)
+				if _, err := strconv.ParseInt(token, 10, 64); err == nil {
+					if !headersCreated {
+						headers = append(headers, strings.Join([]string{token, "m"}, ""))
+						headers = append(headers, strings.Join([]string{token, "s"}, ""))
+					}
+				} else {
+					timingTokens := strings.Split(token, " ")
+					fields = append(fields, strings.TrimLeft(timingTokens[0], "ms"))
+					fields = append(fields, strings.TrimLeft(timingTokens[1], "ms"))
+				}
+			}
+
+			if strings.HasPrefix(line, "28:") {
+				headersCreated = true
+				messageHeaderSent = false
+				records = append(records, fields)
+				fields = make([]string, 0)
+				fmt.Println("Recorded", len(records), "frames")
+			}
+
+		}
+
+	}
+
+	if err := lineScanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/derktes/ir-signal-collector
+
+go 1.14
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
+)
